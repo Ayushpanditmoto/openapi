6a82f3c4818c2980ebda399845832e923064d90f
Ayush Kumar Pandit <ayushpanditmoto@gmail.com>
2023-01-11 04:21:22 +0530

    new build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
+import spinner from "./spinner";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState("");
+  const [image, setImage] = useState("");
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+  const handleClick = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        "https://sparkling-skirt-frog.cyclic.app/openai",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            prompt: search,
+          }),
+        }
+      );
+      const data = await response.json();
+      console.log(data);
+      setImage(data.image);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+
+      console.log(error);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Appcontain>
+      <div className="title">
+        <h2>Open AI Image</h2>
+      </div>
+      <div className="inputwrap">
+        <input
+          type="text"
+          value={search}
+          onChange={handleChange}
+          placeholder="Enter you data"
+        />
+      </div>
+      <div className="btnwrap">
+        <button onClick={handleClick}>Submit</button>
+      </div>
+      {loading && spinner()}
+
+      {image && (
+        <div className="imagewrap">
+          <img src={image} alt="images" />
+        </div>
+      )}
+    </Appcontain>
   );
 }
 
 export default App;
+
+const Appcontain = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  .inputwrap {
+    margin-top: 20px;
+    background-color: #fcfcfc;
+    border-radius: 5px;
+    padding: 10px 20px;
+    width: 400px;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  }
+  .imagewrap {
+    margin-top: 20px;
+    background-color: #fcfcfc;
+    width: 550px;
+    height: 550px;
+  }
+`;
---
+++ src/spinner.js
@@ -0,0 +1,8 @@
+import { BeatLoader } from "react-spinners";
+import React from "react";
+
+function spinner() {
+  return <BeatLoader />;
+}
+
+export default spinner;
